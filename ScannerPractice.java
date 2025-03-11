82a6689cdbfa4cbf44c1a6db81b19e293b710700
JuanEsteban-prog <juan.caicedo@alumnos.uneatlantico.es>
2025-03-11 12:51:32 -0500

    Scanner theory with common errors

--- ScannerPractice.java
+++ ScannerPractice.java
@@ -6,26 +6,47 @@ public class ScannerPractice {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter username: ");
+        System.out.print("Enter your Name: ");
         String name = scanner.nextLine();
 
-        System.out.print("Hello " + name);
-
-        System.out.println("Enter age: ");
+        System.out.print("Enter your age: ");
         int age = scanner.nextInt();
 
-        System.out.println("Your age is: " + age);
-
-        System.out.println("Enter a decimal number: ");
-        double decimal = scanner.nextDouble();
-
-        System.out.println("Your decimal number is: " + decimal);
+        System.out.print("Enter your GPA: ");
+        double gpa = scanner.nextDouble();
 
         System.out.println("Are you a programmer? (true/false): ");
         boolean isProgrammer = scanner.nextBoolean();
 
-        System.out.println("You are a programmer: " + isProgrammer);
+        System.out.print("Hello " + name);
+        System.out.println("Your age is: " + age);
+        System.out.println("Your GPA is : " + gpa);
+
+        if (isProgrammer) {
+            System.out.println("Congratulation you are a programmer");
+        } else {
+            System.out.println("LOL, you are not a programmer HAHAHAHA");
+        }
 
         scanner.close();
     }
+    // COMMON ERRORS
+    // System.out.println("Enter your age: ");
+    // int age = scanner.nextInt();
+
+    // System.out.println("Enter your favorite color: ");
+    // String color = scanner.nextLine();
+    // This will not work because the nextLine() method will read the newline
+    // character in the input buffer and return the rest of the line.
+
+    // To fix this, you can add an extra scanner.nextLine() method to consume the
+    // newline character before calling the nextLine() method to read the color
+    // input.
+
+    // System.out.println("Enter your age: ");
+    // int age = scanner.nextInt();
+    // scanner.nextLine(); // Consume newline character
+
+    // System.out.println("Enter your favorite color: ");
+    // String color = scanner.nextLine()
 }
--- Variables.java
+++ Variables.java
@@ -1,22 +1,22 @@
-class Variables{
+class Variables {
     public static void main(String[] args) {
         // variable = reusable container for a value
-        //            a variables behaves as if it was the value it contains
+        // a variables behaves as if it was the value it contains
 
         // primitive = simple value stored directly in memory (stack)
         // Reference = memory address (stack) that points to the (heap)
 
         // Primitive vs reference
-        // int          string
-        // double       array
-        // char         object
+        // int string
+        // double array
+        // char object
         // boolean
 
-        int year = 2025; 
-        int age = 21; 
+        int year = 2025;
+        int age = 21;
 
         double price = 19.99;
-        double tax = 0.07; 
+        double tax = 0.07;
 
         char grade = 'A';
         char sign = '!';
@@ -25,7 +25,7 @@ class Variables{
         boolean isStudent = true;
         boolean forSale = false;
 
-        if(isStudent){
+        if (isStudent) {
             System.out.println("You are a student");
         } else {
             System.out.println("You are not a student");
