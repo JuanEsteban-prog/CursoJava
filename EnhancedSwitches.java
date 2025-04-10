39858c6792d3f0609eaa7084356df026d75de2ca
JuanEsteban-prog <juan.caicedo@alumnos.uneatlantico.es>
2025-04-10 18:08:02 +0200

    Understanding enhanced switches

---
+++ EnhancedSwitches.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class EnhancedSwitches {
+    public static void main(String[] args) {
+        // Enhanced switch = A replacement to many else if statements
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the day of the week: ");
+        String day = scanner.nextLine();
+
+        switch (day) {
+            case "Monday" -> System.out.println("It is a weekday");
+            case "Tuesday" -> System.out.println("It is a weekday");
+            case "Wednesday" -> System.out.println("It is a weekday");
+            case "Thursday" -> System.out.println("It is a weekday");
+            case "Friday" -> System.out.println("It is a weekday");
+            case "Saturday" -> System.out.println("It is the weekend!!");
+            case "Sunday" -> System.out.println("It is the weekend!!");
+            default -> System.out.println(day + " is not a day");
+        }
+        // It can also be like this:
+        switch (day) {
+            case "Monday", "Tuesday", "Wednesday", "Thursday", "Friday" -> System.out.println("It is a weekday");
+            case "Saturday", "Sunday" -> System.out.println("It is the weekend!!");
+            default -> System.out.println(day + " is not a day");
+
+        }
+    }
+}
