7dada944a797ace47cd817f5b4beafb7cce6160e
JuanEsteban-prog <juan.caicedo@alumnos.uneatlantico.es>
2025-04-12 08:55:31 -0500

    Understanding for loops

---
+++ ForLoops.java
@@ -0,0 +1,19 @@
+class ForLoops {
+    public static void main(String[] args) {
+        // for loop = execute some code a Certain amount of times
+        // for(initalization; condition- when do we want to stop? ; step- increment or
+        // decrement ){
+        //
+        // }
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("pizza");
+        }
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+
+    }
+
+}
