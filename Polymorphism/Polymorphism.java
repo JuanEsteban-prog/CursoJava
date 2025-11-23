829b8f77a16954104510f1a0015aaf2d742712d2
JuanEsteban-prog <juan.caicedo@alumnos.uneatlantico.es>
2025-11-23 22:00:56 +0100

    Understanding the key concepts of polymorphism

---
+++ Polymorphism/Polymorphism.java
@@ -0,0 +1,26 @@
+package Polymorphism;
+
+public class Polymorphism {
+    public static void main(String[] args) {
+        // Polymorphism = "POLY" = "MANY"
+        // "MORPH" = "SHAPE"
+        // Objects can identify as other objects
+        // Objects can be treated as objects of common superclass.
+
+        Car car = new Car();
+        Bike bike = new Bike();
+        Boat boat = new Boat();
+
+        car.go();
+        bike.go();
+        boat.go();
+
+        Vehicle[] vehicles = { car, bike, boat };
+
+        for (Vehicle vehicle : vehicles) {
+            vehicle.go();
+        }
+
+        // Polymorphism can be achived to by using Interfaces!
+    }
+}
